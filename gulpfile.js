f89a85f8d6031e3b30092904f175d71ecef4b913
Guido Granobles <guidogranobles@users.noreply.github.com>
2016-05-27 13:16:12 -0600

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+
+gulp.task('copy3party', function() {
+
+    gulp.src('node_modules/rxjs/**/*')
+   .pipe(gulp.dest('app/scripts/thirdparty/rxjs'));
+   
+    gulp.src('node_modules/angular2-in-memory-web-api/**/*')
+   .pipe(gulp.dest('app/scripts/thirdparty/angular2-in-memory-web-api'));
+   
+      gulp.src('node_modules/@angular/**/*')
+   .pipe(gulp.dest('app/scripts/thirdparty/@angular'));   
+   
+});
