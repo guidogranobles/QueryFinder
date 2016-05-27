5fd7c91519a44d74dc03946abcbd1423f2c4951e
Guido Granobles <guidogranobles@users.noreply.github.com>
2016-05-27 13:08:37 -0600

    Create systemjs.js

---
+++ app/conf/systemjs.js
@@ -0,0 +1,51 @@
+/**
+ * System configuration for Angular 2 samples
+ * Adjust as necessary for your application needs.
+ * Override at the last minute with global.filterSystemConfig (as plunkers do)
+ */
+(function(global) {
+
+  // map tells the System loader where to look for things
+  var map = {
+    'app':                        'scripts', // 'dist',
+    'rxjs':                       'scripts/thirdparty/rxjs',
+    'angular2-in-memory-web-api': 'scripts/thirdparty/angular2-in-memory-web-api',
+    '@angular':                   'scripts/thirdparty/@angular',
+    'prettify':                   'scripts/thirdparty/google-code-prettify'
+  };
+
+  // packages tells the System loader how to load when no filename and/or no extension
+  var packages = {
+    'app':                        { main: 'main.js',  defaultExtension: 'js' },
+    'rxjs':                       { defaultExtension: 'js' },
+    'angular2-in-memory-web-api': { defaultExtension: 'js' },
+    'prettify':                   { prettify: 'prettify.js',  defaultExtension: 'js' }
+  };
+
+  var packageNames = [
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/core',
+    '@angular/http',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    '@angular/router-deprecated',
+    '@angular/testing',
+    '@angular/upgrade',
+  ];
+
+  // add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
+  packageNames.forEach(function(pkgName) {
+    packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
+  });
+
+
+
+  var config = {
+    map: map,
+    packages: packages
+  }
+
+  System.config(config);
+
+})(this);
