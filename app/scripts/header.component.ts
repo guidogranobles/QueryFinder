40d45a7552bf1883f03190d386300a0e091b2332
Guido Granobles <guidogranobles@users.noreply.github.com>
2016-06-02 14:14:14 -0600

    Update header.component.ts

--- app/scripts/header.component.ts
+++ app/scripts/header.component.ts
@@ -1,8 +1,42 @@
-import {Component} from '@angular/core';
+import {Component, Output, EventEmitter} from '@angular/core';
 
 @Component({ 
 	selector: 'my-header',
 	templateUrl: 'html/header.html'
 })
+export class Header {
 
-export class Header {}
+    activities  = {
+        add: {
+            action: "Add",
+            label: "Create a new sql query"
+        },
+        edit:{
+            action: "Edit",
+            label: "Edit query"
+        },
+        search: {
+            action: "Search",
+            label: "Search results"
+        },
+        favorites: {
+            action: "Favorites",
+            label: "Your favorite queries"
+        },
+        settings: {
+            action: "Settings",
+            label: "Enviroment settings"
+        }
+    };
+
+    @Output() addMenuClick: EventEmitter<any> = new EventEmitter();
+
+    public addOnClick() {
+        this.addMenuClick.emit({value: this.activities.add});
+    }
+
+    public searchOnClick() {
+        this.addMenuClick.emit({value: this.activities.search});
+    }
+
+}
