31a00e79a24a3ecec72df4cfe2d27dfad0046e8c
Guido Granobles <guidogranobles@users.noreply.github.com>
2016-06-02 14:15:00 -0600

    Update qFinder.service.ts

--- app/scripts/qFinder.service.ts
+++ app/scripts/qFinder.service.ts
@@ -2,9 +2,9 @@
  * Created by u334244 on 5/24/16.
  */
 import { Injectable }     from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers, RequestOptions  } from '@angular/http';
 import { Observable }     from 'rxjs/Rx';
-import {SQLQuery}  from './sqlQuery.model';
+import {SQLQuery}         from './sqlQuery.model';
 
 
 
@@ -13,58 +13,70 @@ export class QFinderService {
 
     constructor (private http: Http) {}
 
-    private serviceUrl = {
-               getAll: '/qfinder/all',
-               getByDesc: '/qfinder/description/',
-               getByQuery: '/qfinder/query/',
-               getByTags: '/qfinder/tags/',
-               getByAuthor: '/qfinder/author/',
-               getByTeams: '/qfinder/filter/teams?',
-               postNewOne: '/qfinder/create',
-               updateOne: '/qfinder/update',
-               deleteOne: '/qfinder/delete/',
-               getByFilter: '/qfinder/filter?'
+    ROOT_APP: string = '/qfinder';
+
+    private servicePath = {
+               getAll:     this.ROOT_APP + '/all',
+               getByDesc:  this.ROOT_APP + '/description/',
+               getByQuery: this.ROOT_APP + '/query/',
+               getByTags:  this.ROOT_APP + '/tags/',
+               getByAuthor:this.ROOT_APP + '/author/',
+               getByTeams: this.ROOT_APP + '/filter/teams?',
+               postNewOne: this.ROOT_APP + '/create',
+               updateOne:  this.ROOT_APP + '/update',
+               deleteOne:  this.ROOT_APP + '/delete/',
+               getByFilter:this.ROOT_APP + '/filter?'
     };
 
     getAll (): Observable<SQLQuery[]> {
-        return this.http.get(this.serviceUrl.getAll)
+        return this.http.get(this.servicePath.getAll)
             .map(this.extractData)
             .catch(this.handleError);
     }
 
     getByTeams(teams: string): Observable<SQLQuery[]> {
-        return this.http.get(this.serviceUrl.getByTeams + 'teams=' + teams)
+        return this.http.get(this.servicePath.getByTeams + 'teams=' + teams)
             .map(this.extractData)
             .catch(this.handleError);
     }
 
     getByDescription (desc: string): Observable<SQLQuery[]> {
-        return this.http.get(this.serviceUrl.getByDesc + desc)
+        return this.http.get(this.servicePath.getByDesc + desc)
                                 .map(this.extractData)
                                 .catch(this.handleError);
     }
 
     getByQuery (query: string): Observable<SQLQuery[]> {
-        return this.http.get(this.serviceUrl.getByQuery + query)
+        return this.http.get(this.servicePath.getByQuery + query)
             .map(this.extractData)
             .catch(this.handleError);
     }
 
     getByTags (tags: string): Observable<SQLQuery[]> {
-        return this.http.get(this.serviceUrl.getByTags + tags)
+        return this.http.get(this.servicePath.getByTags + tags)
             .map(this.extractData)
             .catch(this.handleError);
     }
 
     getByAuthor (author: string): Observable<SQLQuery[]> {
-        return this.http.get(this.serviceUrl.getByAuthor + author)
+        return this.http.get(this.servicePath.getByAuthor + author)
             .map(this.extractData)
             .catch(this.handleError);
     }
 
     getByFullFilters(exp: string, fields: string, teams: string): Observable<SQLQuery[]> {
         console.log('send: ' + fields);
-        return this.http.get(this.serviceUrl.getByFilter + 'term=' + exp + '&' + 'fields=' + fields + '&' + 'teams=' + teams)
+        return this.http.get(this.servicePath.getByFilter + 'term=' + exp + '&' + 'fields=' + fields + '&' + 'teams=' + teams)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    save(newQuery: any): Observable<SQLQuery[]> {
+        let body = JSON.stringify(newQuery);
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+
+        return this.http.post(this.servicePath.postNewOne, body, options)
             .map(this.extractData)
             .catch(this.handleError);
     }
