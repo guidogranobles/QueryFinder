26619093387b6c264e720d9b4fe83a6202bcf794
Guido Granobles <guidogranobles@users.noreply.github.com>
2016-06-02 14:13:41 -0600

    Update content.component.ts

--- app/scripts/content.component.ts
+++ app/scripts/content.component.ts
@@ -1,29 +1,30 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild , Input   } from '@angular/core';
 import { NgForm }    from '@angular/common';
 import { NgStyle }    from '@angular/common';
 
-import {Sidebar} from './sidebar.component';
-import {PrettifyDirective} from './prettify.directive';
-import {QFinderService} from './qFinder.service';
 import {SQLQuery}  from './sqlQuery.model';
+import {FrmSearchResult}  from './frmSearchResults.component';
+import {FrmAddQuery}  from './frmAddQuery.component';
 
 @Component({ 
 	selector: 'my-content',
 	templateUrl: 'html/content.html',
-	directives: [Sidebar, PrettifyDirective, NgStyle],
-    providers: [QFinderService]
+	directives: [NgStyle, FrmSearchResult, FrmAddQuery]
+
 })
 export class Content {
 
-    constructor(private qFinderService: QFinderService){}
+    constructor(){
+
+    }
+
+    @ViewChild(FrmSearchResult) frmSearchResult:FrmSearchResult;
 
     fieldFilters = {description: {field: 'Description', label:'description', selected: false},
-                    query: {field: 'Query',  label:'query', selected: false}, tags: { field: 'Tags',  label:'tags', selected: false},
-                    author: { field: 'Author',  label:'author', selected: false } };
+        query: {field: 'Query',  label:'query', selected: false}, tags: { field: 'Tags',  label:'tags', selected: false},
+        author: { field: 'Author',  label:'author', selected: false } };
 
-    teamFilters = { teamAppManag: { label:'Application Management', selected: false}, teamClosFun: { label: 'Closing/Funding', selected: false},
-                                    teamDocGenp: { label:'Doc Gen / Prepaids', selected: false}, teamPricingFees: { label: 'Pricing - Closing Fees', selected: false} };
-
+   
     txtSearch: string = '';
     errorMessage: string;
     errorMsgVisible: boolean = false;
@@ -31,160 +32,61 @@ export class Content {
     titleHeader: string = '';
     backgroundHeader: string = '';
     footerMsg: string = '';
-    sqlQueries: SQLQuery[];
+    showMsg = this.showMessage.bind(this);
+    marginResults: string = '0';
+    transResults: string = '';
 
-     findQuery(){
+    _action: any = {
+           action: "Search",
+           label: "Search results"
+    };
 
-        if(this.sqlQueries){
-            this.sqlQueries.length = 0;
-        }
+    test(t: any){
+        this._action = t;
+    }
 
-        this.errorMessage = "";
+   @Input()
+    set action(act: any){
+        this._action = act;
+        console.log(act);
+    }
 
-        var selectedFieldFilters = this.getSelectedFilters(this.fieldFilters);
-        var selectedTeamFilters = this.getSelectedFilters(this.teamFilters);
+    get action(){
+        return this._action;
+    }
 
-        if(selectedFieldFilters.length === 1 && selectedTeamFilters.length ===0 && this.txtSearch.length > 0){
-            this.execFiltersByFieldOnly(selectedFieldFilters);
-        }else if(selectedFieldFilters.length === 0 && selectedTeamFilters.length > 0 && this.txtSearch.length === 0){
-            this.getByTeamsOnly(this.getStringItemsFromArray(selectedTeamFilters));
-        }else if( (selectedFieldFilters.length > 0 || selectedTeamFilters.length > 0) && this.txtSearch.length > 0){
-            this.getByFullFilters(this.txtSearch, this.getStringItemsFromArray(selectedFieldFilters), this.getStringItemsFromArray(selectedTeamFilters));
-        }else if(selectedFieldFilters.length === 0 && selectedTeamFilters.length === 0 && this.txtSearch.length === 0){
-            this.getAll();
+    public findQuery(){
+        this.frmSearchResult.findQuery(this.txtSearch, this.fieldFilters, this.teamFilters, this.showMsg);
+    }
+
+    public isSearchQueryActive(){
+
+        var isSearchSelected = this._action.action === 'Search'
+        if(isSearchSelected){
+            this.moveResultsPanel('right');
         }else{
-            this.errorHandler('The search parameters are wrong. Please fix and try again');
+            this.moveResultsPanel('left');
         }
 
-        if(this.sqlQueries && this.sqlQueries.length === 0 && this.errorMessage.length === 0){
-            this.errorHandler('No results found');
-        }
-
+        return isSearchSelected;
     }
 
-    private execFiltersByFieldOnly(selectedFieldFilters: string[]){
-
-        if(this.isOnlyThisFilterSelected('description', selectedFieldFilters)){
-            this.getByDescription(this.txtSearch);
-        }else if(this.isOnlyThisFilterSelected('query', selectedFieldFilters)){
-            this.getByQuery(this.txtSearch);
-        }else if(this.isOnlyThisFilterSelected('tags', selectedFieldFilters)){
-            this.getByTags(this.txtSearch);
-        }else if(this.isOnlyThisFilterSelected('author', selectedFieldFilters)){
-            this.getByAuthor(this.txtSearch);
-        }
-
+    public isAddNewQueryActive(){
+        return this._action.action === 'Add';
     }
 
-    private getAll() {
-        this.qFinderService.getAll()
-            .subscribe(
-                sqlQueries => this.responseProcessor(sqlQueries),
-                error =>   this.errorHandler( <any>error)
-        );
+    private moveResultsPanel(direction: string){
+
+         if(direction === 'right'){
+             this.marginResults = '0';
+         }else  if(direction === 'left'){
+             this.marginResults = '250px'
+
+         }
     }
 
-    private getByFullFilters(exp: string, fields: string, teams: string) {
-        console.log('getByFullFilters' + fields);
-        this.qFinderService.getByFullFilters(exp, fields, teams)
-            .subscribe(
-            sqlQueries => this.responseProcessor(sqlQueries),
-            error =>   this.errorHandler( <any>error)
-        );
-    }
 
-    private getByTeamsOnly(teams: string) {
-        this.qFinderService.getByTeams(teams)
-            .subscribe(
-            sqlQueries => this.responseProcessor(sqlQueries),
-            error =>  this.errorHandler( <any>error)
-        );
-    }
-
-    private getByDescription(desc: string) {
-        this.qFinderService.getByDescription(desc)
-            .subscribe(
-            sqlQueries => this.responseProcessor(sqlQueries),
-            error =>  this.errorHandler( <any>error)
-        );
-    }
-
-    private getByQuery(query: string) {
-        this.qFinderService.getByQuery(query)
-            .subscribe(
-            sqlQueries => this.responseProcessor(sqlQueries),
-            error =>   this.errorHandler( <any>error)
-        );
-    }
-
-    private getByTags(tags: string) {
-        this.qFinderService.getByTags(tags)
-            .subscribe(
-            sqlQueries => this.responseProcessor(sqlQueries),
-            error =>  this.errorHandler( <any>error)
-        );
-    }
-
-    private getByAuthor(author: string) {
-        this.qFinderService.getByAuthor(author)
-            .subscribe(
-            sqlQueries => this.responseProcessor(sqlQueries),
-            error =>  this.errorHandler( <any>error)
-        );
-    }
-
-    private getSelectedFilters(filters: Object){
-
-        var selectedFilters: Array<string> = [];
-
-        Object.keys(filters).filter(function(key) {
-            if(filters.hasOwnProperty(key)){
-                if(filters[key].selected)
-                    selectedFilters.push(filters[key].label);
-            }
-
-            return true;
-         });
-
-        return selectedFilters;
-    }
-
-    private getStringItemsFromArray(array: String[]){
-
-        var result = '';
-
-        array.forEach(function(item, index){
-            result = result + item;
-            if(index <= array.length-2){
-                result = result + ',';
-            }
-        });
-
-        return result;
-    }
-
-    private isOnlyThisFilterSelected(filterName: string, selectedFilters: string[]){
-        if(selectedFilters.length === 1 && selectedFilters.indexOf(filterName) > -1){
-            return true;
-        }
-        return false;
-    }
-
-    private errorHandler(error: string){
-         this.errorMessage = error;
-         this.showMessage('Error');
-    }
-
-    private responseProcessor(results: SQLQuery[]){
-        if(results && results.length === 0){
-            this.errorMessage = 'No results found';
-            this.showMessage('Info');
-        }else{
-            this.sqlQueries = results;
-        }
-    }
-
-    private showMessage(typeMessage: string){
+    private showMessage(typeMessage: string, msg: string){
         if(typeMessage === 'Error'){
             this.titleHeader = 'Error!';
             this.backgroundHeader = "rgba(255, 0, 0, 0.39)";
@@ -195,8 +97,11 @@ export class Content {
             this.footerMsg = 'The server returned 0 records';
         }
 
+        this.errorMessage = msg;
+
         this.opValue='dissapear 2s linear 0s 2 alternate';
         var pr = this;
         setTimeout(function(){ pr.opValue = "";}, 5000);
     }
+
 }
